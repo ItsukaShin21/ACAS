7a49ede96bf6de2c329dcc6b8117a20cccea67bf
ItsukaShin21 <jef.lotoc@gmail.com>
2023-11-17 22:24:22 -0800

    resets all file

--- ACAS/dbconnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost";
-     $username = "root";
-     $password = "";
-     $dbname = "acas";
-
-     $connection = new mysqli($servername, $username, $password, $dbname);
-?>
